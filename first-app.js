9bd777f796b897e7a202243c55c36fe067f8a0c4
pargati rastogi <pragati.r@nimbuspost.com>
2023-07-11 13:36:30 +0530

    first commit

---
+++ first-app.js
@@ -0,0 +1,6 @@
+// console.log('hello from node.js');
+
+const fs = require('fs');//fetching predefine file system of node js
+
+fs.writeFileSync('newFile.txt','Hello frome node.js');// this will create new file with text in it.
+
